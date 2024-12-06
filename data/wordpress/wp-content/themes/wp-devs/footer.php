e894368335599c19d627bbdeb577720d268cadbe
Nathalia <nathaliamend017@gmail.com>
2024-12-05 22:32:25 -0400

    pages especificas criadas

--- data/wordpress/wp-content/themes/wp-devs/footer.php
+++ data/wordpress/wp-content/themes/wp-devs/footer.php
@@ -1,14 +1,14 @@
 <footer class="site-footer">
-        <div class="container">
-            <div class="copyright">
-                <p>Copyright X - All Rights Reserved</p>
-            </div>
-            <nav class="footer-menu">
-                <?php wp_nav_menu(array('theme_location' => 'wp_devs_footer_menu', 'depth' => 1)); ?>
-            </nav>
+    <div class="container">
+        <div class="copyright">
+            <p>Copyright X - All Rights Reserved</p>
         </div>
-    </footer>
-    <?php wp_footer();?>
+        <nav class="footer-menu">
+            <?php wp_nav_menu(array('theme_location' => 'wp_devs_footer_menu', 'depth' => 1)); ?>
+        </nav>
+    </div>
+</footer>
+<?php wp_footer(); ?>
 </body>
 
 </html>
